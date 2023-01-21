1c9b5e3d77e7c8388ce45d2b34c6aad3f976fee4
Eclipse175 <fahimkhobar2004@gmail.com>
2023-01-21 13:23:53 +0300

    added some styling and javascript and copy button

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const InputField = document.getElementById('input-field');
 const randomSlider = document.getElementById('slider');
+const copyPassword = document.querySelector('button');
 
-InputField.addEventListener('keypress', (e) =>{
+var alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+ 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+function randomPassword() {
+    let random = alphabets[Math.floor(Math.random()*alphabets.length)]
+    joinedAlphabets += random
+    console.log(random);
+}
+randomPassword();
+
+InputField.addEventListener('keypress', (e) => {
     if (e.key === "Enter") {
         e.preventDefault();
         InputField.value = "";
